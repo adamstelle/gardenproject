ed96dcb0236def92cc5c68e7f646edc18d8ee166
Adam Stelle <adamstelle@gmail.com>
2015-01-15 16:07:41 -0800

    Added functionality for choosing specific locations

--- scripts.js
+++ scripts.js
@@ -14,23 +14,40 @@ function Plant(name, nutrients, sunlight, water, ripeTime) {
 }  
 
 // Create environment object to store dailyValues, and call environment
-var dailyValue = function() {
-  dailyNutrients = Math.floor(Math.random() * (10 - 1)) + 1;
-  dailySunlight = Math.floor(Math.random() * (10 - 1)) + 1;
-  dailyWater = Math.floor(Math.random() * (10 - 1)) + 1;
+function Region(name, minNutrients, maxNutrients, minSunlight, maxSunlight, minWater, maxWater) {
+  this.name = name;
+  this.minNutrients = minNutrients;
+  this.maxNutrients = maxNutrients;
+  this.minSunlight = minSunlight;
+  this.maxSunlight = maxSunlight;
+  this.minWater = minWater;
+  this.maxWater = maxWater;
+}
+
+Region.prototype.dailyValue = function() {
+  dailyNutrients = Math.floor(Math.random() * (this.maxNutrients - this.minNutrients)) + this.minNutrients;
+  dailySunlight = Math.floor(Math.random() * (this.maxSunlight - this.minSunlight)) + this.minSunlight;
+  dailyWater = Math.floor(Math.random() * (this.maxWater - this.minWater)) + this.minWater;
 }
 
 function Garden() {
   this.plants = [];
 }
 
+// function displayContent() {
+//   // Use checkboxed
+//   // if "fern" checked
+//   // plants.push(fern)
+// }
+
 Garden.prototype.addPlant = function(name, nutrients, sunlight, water, ripeTime) {
   this.plants.push(new Plant(name, nutrients, sunlight, water, ripeTime));    
 }
 
-Garden.prototype.growPlants = function() {
+Garden.prototype.growPlants = function(location) {
+  this.location = location;
   for(var day = 0; day < 10; day++) {
-    dailyValue();
+    location.dailyValue();
     for(var i = 0; i < this.plants.length; i++) {
       if ((this.plants[i].nutrients >= dailyNutrients) && (this.plants[i].sunlight >= dailySunlight) && (this.plants[i].water >= dailyWater)) {
         this.plants[i].growingDays.push(1);
@@ -43,12 +60,22 @@ Garden.prototype.growPlants = function() {
       };
     };
   };
+  console.log(location);
   console.log(this.plants);
 };
 
+var northwest = new Region("northwest", 2,8,  1,6,  8,10);
+var west      = new Region("west",      3,8,  5,10, 4,7);
+var southwest = new Region("southwest", 2,6,  9,10, 1,5);
+var midwest   = new Region("midwest",   3,7,  4,7,  5,8);
+var southeast = new Region("southeast", 5,8,  4,7,  7,9);
+var northeast = new Region("northeast", 4,7,  1,7,  2,9);
+
 var plants = new Garden();
-plants.addPlant("Fern", 8, 8, 9, 20);
+
+// var fern = new Plant("Fern",9,6,9,20);
+plants.addPlant("Fern", 9, 6, 9, 20);
 plants.addPlant("Rose", 8, 7, 5, 30);
 plants.addPlant("Cabbage", 7, 6, 8, 40);
-plants.growPlants();
+plants.growPlants(west);
 
